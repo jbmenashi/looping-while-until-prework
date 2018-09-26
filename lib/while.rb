616633ec939401d5b54aec9029182b1955684035
Jacob Menashi <jbmenashi@gmail.com>
2018-09-26 17:01:13 +0000

    automatically backed up by learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,11 +1,8 @@
-require 'pry'
-
 def using_while
 	levitation_force = 6
 	while levitation_force < 11 
 	  puts "Wingardium Leviosa"
-	  levitation_force += 1
-	  binding.pry
+	  levitation_force += 1 
 	end
 end
 
